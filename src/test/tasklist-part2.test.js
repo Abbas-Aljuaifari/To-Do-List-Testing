e946543dc378027bf669007a51e5224a7272fffc
Henok Kirubel <henokhm2@gmail.com>
2023-06-23 16:44:44 +0300

    Fix failing tests

--- src/test/tasklist-part2.test.js
+++ src/test/tasklist-part2.test.js
@@ -57,10 +57,10 @@ describe('TaskList', () => {
     it('should not edit the description if the index is out of range', () => {
       const taskList = new TaskList();
       taskList.addTask('Task 1');
-      const index = 1;
+      const index = 2;
       const newDescription = 'Updated Task 1';
       taskList.editTaskDescription(index, newDescription);
-      expect(taskList.tasks[0].description).not.toBe(newDescription);
+      expect(taskList.tasks[0].description).toEqual('Task 1');
     });
   });
 
@@ -69,11 +69,25 @@ describe('TaskList', () => {
       const taskList = new TaskList();
       taskList.addTask('Task 1');
       taskList.addTask('Task 2');
+      taskList.addTask('Task 3');
       taskList.tasks[0].completed = true;
+      taskList.tasks[2].completed = true;
+
       taskList.clearCompleted();
-      const expectedTasks = [new Task('Task 2', false)];
+
+      const expectedTasks = [new Task('Task 2')];
+      expectedTasks[0].index = 1;
+
       expect(taskList.tasks.length).toBe(expectedTasks.length);
-      expect(taskList.tasks).toEqual(expectedTasks);
+      expect(taskList.tasks).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            description: 'Task 2',
+            completed: false,
+            index: 1,
+          }),
+        ]),
+      );
     });
   });
 });
